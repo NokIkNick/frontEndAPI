1fff7f0a8d73a45acdde88bf89a6b798bd7c2bbb
Nicklas W <Nicklas2002@hotmail.com>
2024-06-21 03:03:53 +0200

    navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 import {Home} from "./page/Home"
 import {Layout} from "./layout/Layout"
 import { PageNotFound } from "./page/PageNotFound"
+import { About } from "./page/About"
+import { Skills } from "./page/Skills"
+import { Education } from "./page/Education"
+import { Projects } from "./page/Projects"
+import { Experience } from "./page/Experience"
+import { Contact } from "./page/Contact"
 
 
 
@@ -15,6 +21,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout/>}>
                 <Route index element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/skills" element={<Skills/>}/>
+                <Route path="/education" element={<Education/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+                <Route path="/experience" element={<Experience/>}/>
+                <Route path="/contact" element={<Contact/>}/>
             </Route>
             <Route path="*" element={<PageNotFound/>}/>
           </Routes>
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,22 +1,148 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const NavigationBar = styled.nav`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: center;
+
+    @media (min-width: 768px){
+        flex-wrap: nowrap;
+    
+    }
+`;
+
+const Logo = styled.div`
+    display: none;
+    justify-content: center;
+    padding: 1vh;
+    width: 100%;
+    font-size: 1.3rem;
+
+    @media (min-width: 768px){
+      display: flex;
+      color: var(--grey);
+      width: auto;
+      margin: 1vh;
+    }
+`;
+
+const MenuIcon = styled.div`
+    display: block;
+    cursor: pointer;
+    font-size: 1.8rem;
+    padding: 1vh;
+    color: var(--grey);
+
+    @media (min-width: 768px){
+        display: none;
+    }
+`;
+
+const StyledUl = styled.ul`
+    display: flex;
+    list-style: none;
+    width:100%;
+    justify-content: space-around;
+    padding: 0;
+    margin: 0;
+    flex-wrap: wrap;
+    flex-direction: column;
+    
+    @media (min-width: 768px){
+        flex-direction: row;
+        justify-content: flex-end;
+    }
+`;
+
+const StyledLi = styled.li`
+    display: ${({isOpen}) => isOpen ? 'flex' : 'none'};  
+    flex:1;
+    justify-content: center;
+    padding: 1%;
+
+    @media (min-width: 768px){
+        display: flex;
+        flex: 0;
+    }
+`;
+
+const StyledNavlink = styled(NavLink)`
+    text-decoration: none;
+    padding: 1vh;
+    font-size: 100%;
+    display: ${({isOpen}) => isOpen ? 'block' : 'none'};  
+    width: 100%;
+    
+    @media (min-width: 768px){
+        display: flex;
+        font-size: 120%;
+
+        &.active{
+        text-decoration: underline;
+        text-underline-offset: 2vh;
+    }
+    }
+
+    &:visited{
+        color: var(--grey);
+    }
+
+    &:hover{
+        color: var(--teal);
+    }
+
+    &:active{
+        color: var(--light-blue);
+    }
+
+    &.active{
+      text-decoration: underline;
+      text-underline-offset: 1vh;
+    }
+
+
+`;
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                    <NavLink to="/skills">Skills</NavLink>
-                    <NavLink to="/education">Education</NavLink>
-                    <NavLink to="/projects">Projects</NavLink>
-                    <NavLink to="/experience">Experience</NavLink>  
-                    <NavLink to="/contact">Contact</NavLink>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    <Container>
+        <NavigationBar>
+            <Logo id='logo'>Nicklas Winther</Logo>
+            <MenuIcon onClick={() => setIsOpen(!isOpen)}>☰</MenuIcon>
+            <StyledUl>
+                <StyledLi isOpen={isOpen}>
+                  <StyledNavlink isOpen={isOpen} to="/">Home</StyledNavlink>
+                </StyledLi>
+                <StyledLi isOpen={isOpen}>  
+                  <StyledNavlink isOpen={isOpen} to="/about">About</StyledNavlink>
+                </StyledLi>
+                <StyledLi isOpen={isOpen}>
+                  <StyledNavlink isOpen={isOpen} to="/skills">Skills</StyledNavlink>
+                </StyledLi>
+                <StyledLi isOpen={isOpen}>
+                  <StyledNavlink isOpen={isOpen} to="/education">Education</StyledNavlink>
+                </StyledLi>
+                <StyledLi isOpen={isOpen}>
+                  <StyledNavlink isOpen={isOpen} to="/projects">Projects</StyledNavlink>
+                </StyledLi>
+                <StyledLi isOpen={isOpen}>
+                  <StyledNavlink isOpen={isOpen} to="/experience">Experience</StyledNavlink>
+                </StyledLi>
+                <StyledLi isOpen={isOpen}>  
+                  <StyledNavlink isOpen={isOpen} to="/contact">Contact</StyledNavlink>
+                </StyledLi>
+            </StyledUl>
+        </NavigationBar>
+    </Container>
   )
 }
